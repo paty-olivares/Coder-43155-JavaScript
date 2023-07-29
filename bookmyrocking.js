e0e3056fdd450aeca4a471baed9fe456f4f2dd51
paty-olivares <patricia.olivares@gmail.com>
2023-07-29 10:10:03 -0600

    Coder43155-JavaScript: Entrega 2 29-07-2023

--- bookmyrocking.js
+++ bookmyrocking.js
@@ -1,100 +1,39 @@
-  //Definicion de Variables
-  let qosta = document.getElementById('qosta');
-  let thehives = document.getElementById('thehives');
-  let thefoo = document.getElementById('thefoo');
-  
-  let boleto1 = 0;
-  let boleto2 = 0;
-  let boleto3 = 0;
-  let cuentaBoleto1 = 0;
-  let cuentaBoleto2 = 0;
-  let cuentaBoleto3 = 0;
-  let cuentBoletos = 0;
-  let totalCompra =0;
 
-  //Controlando los Checked Buttons
-  //Y mostrando los resultados de la compra
- qosta.addEventListener('click', function() {
-    if(qosta.checked) {
-      alert("El boleto para ver a los Queen Of Stone Age tiene un valor de MXN $2,500.00")
-      boleto1 = 2500; 
-      cuentaBoleto1 = 1;
-      totalCompra = boleto1 + boleto2 + boleto3;
-      cuentBoletos = cuentaBoleto1 + cuentaBoleto2 + cuentaBoleto3;
-
-      document.getElementById('totalCompra').value = new Intl.NumberFormat('es-MX').format(totalCompra);
-      document.getElementById('totalBoletos').value = new Intl.NumberFormat('es-MX').format(cuentBoletos);
-
-    }
-    else {
-        boleto1 = 0; 
-        cuentaBoleto1 = 0;
-        totalCompra = boleto1 + boleto2 + boleto3;
-        cuentBoletos = cuentaBoleto1 + cuentaBoleto2 + cuentaBoleto3;
-  
-        document.getElementById('totalCompra').value = new Intl.NumberFormat('es-MX').format(totalCompra);
-        document.getElementById('totalBoletos').value = new Intl.NumberFormat('es-MX').format(cuentBoletos);
-  
-    }
-    
-    ; });
-
-    thehives.addEventListener('click', function() {
-        if(thehives.checked) {
-          alert("El boleto para ver a The Hives tiene un valor de MXN $1000.00")
-          boleto2 = 1000;
-          cuentaBoleto2 = 1;
-
-          totalCompra = boleto1 + boleto2 + boleto3;
-          cuentBoletos = cuentaBoleto1 + cuentaBoleto2 + cuentaBoleto3;
-    
-          document.getElementById('totalCompra').value = new Intl.NumberFormat('es-MX').format(totalCompra);
-          document.getElementById('totalBoletos').value = new Intl.NumberFormat('es-MX').format(cuentBoletos);
-
-        } else{
-          
-                boleto2 = 0; 
-                cuentaBoleto2 = 0;
-                totalCompra = boleto1 + boleto2 + boleto3;
-                cuentBoletos = cuentaBoleto1 + cuentaBoleto2 + cuentaBoleto3;
-          
-                document.getElementById('totalCompra').value = new Intl.NumberFormat('es-MX').format(totalCompra);
-                document.getElementById('totalBoletos').value = new Intl.NumberFormat('es-MX').format(cuentBoletos);
-
-        }; });
-
-        thefoo.addEventListener('click', function() {
-            if(thefoo.checked) {
-              alert("El boleto para ver a The Foo Fighters tiene un valor de MXN $3,000.00")
-              boleto3 = 3000;
-              cuentaBoleto3 = 1;
-
-              totalCompra = boleto1 + boleto2 + boleto3;
-              cuentBoletos = cuentaBoleto1 + cuentaBoleto2 + cuentaBoleto3;
-        
-              document.getElementById('totalCompra').value = new Intl.NumberFormat('es-MX').format(totalCompra);
-              document.getElementById('totalBoletos').value = new Intl.NumberFormat('es-MX').format(cuentBoletos);
-
-              
-            }else{
-
-                boleto3 = 0; 
-                cuentaBoleto3 = 0;
-                totalCompra = boleto1 + boleto2 + boleto3;
-                cuentBoletos = cuentaBoleto1 + cuentaBoleto2 + cuentaBoleto3;
-          
-                document.getElementById('totalCompra').value = new Intl.NumberFormat('es-MX').format(totalCompra);
-                document.getElementById('totalBoletos').value = new Intl.NumberFormat('es-MX').format(cuentBoletos);
-
-            }; });
-   
-   
   //Funcion para Manejo de las imagenes
-            function mostrarBoletos(){
+function mostrarBoletos(){
                 
-                let boletos = parseInt(document.getElementById("totalBoletos").value);
-               
-               
+    //Definicion de Variables
+    let qosta =  parseInt(document.getElementById('qosta').value);
+    let thehives =  parseInt(document.getElementById('thehives').value);
+    let thefoo =  parseInt(document.getElementById('thefoo').value);    
+
+    let precioBoleto1 = 2500;
+    let precioBoleto2 = 1500;
+    let precioBoleto3 = 3000;
+  
+    if (validarCampos() == 1) {
+      alert("[WARINING] Asegúrate que todos tus datos estén completos, de otra forma no se puede procesar la compra");
+      return;
+    };
+
+    let totalBoletos = qosta + thehives + thefoo;
+    let totalCompra =  (precioBoleto1 * qosta ) + ( precioBoleto2 * thehives) + ( precioBoleto3 * thefoo );
+    
+    document.getElementById('totalCompra').value = totalCompra;
+    document.getElementById('totalBoletos').value = totalBoletos;
+
+    //dando formato bonito a mis resultados
+    document.getElementById('totalCompra').value = new Intl.NumberFormat('es-MX').format(totalCompra);
+    document.getElementById('totalBoletos').value = new Intl.NumberFormat('es-MX').format(totalBoletos);
+      
+    //validaciones
+    if (totalBoletos == 0) {
+      alert("Debes seleccionar al menos 1 boleto, Favor revisar nuevamente");
+      return;
+    }
+    
+    alert("En hora buena!!! Has adquirido  [" + totalBoletos + "] boletos para ver a tu banda favorita !!!")
+
                 var imgArray = new Array();
 
                 imgArray[0] = new Image();
@@ -110,23 +49,63 @@
                 imgArray[3].src = './img/foo.jpg';               
 
                 var i = 0;
-                alert("En hora buena!!! Has adquirido  " + boletos + " boletos para ver a tu banda favorita !!!")
-                for (i = 0; i < boletos; i++) {
+            
+                for (i = 0; i < totalBoletos; i++) {
 
-                    if (document.getElementById('qosta').checked) {                       
+                    if ( qosta > 0 ) {                       
                         document.getElementById("boleto1").src = imgArray[1].src;
                       };
-                      if (document.getElementById('thehives').checked) {
+                      if ( thehives > 0) {
                          document.getElementById("boleto2").src = imgArray[2].src;
                       };
-                      if (document.getElementById('thefoo').checked) {                        
+                      if (thefoo > 0) {                        
                         document.getElementById("boleto3").src = imgArray[3].src;
                       }
 
                 }
-            }
+};
 //Limpiar la pantalla
-            function limpiarPantalla(){
-                window.location.reload();
-            }
-   
+function limpiarPantalla(){
+       window.location.reload();
+ };
+function validarCampos(){
+     //Validaciones
+     let nombre = document.getElementById('nombre').value;
+     let apellido = document.getElementById('apellido').value;
+     let email = document.getElementById('email').value;     
+     if ( nombre.length == 0 || apellido.length == 0 || email.length == 0  )    
+     {     
+     return 1;
+    }
+    else {
+        return 0;
+    }
+};
+//Funcion para enviar los boletos por correo - In progress
+function enviarEmail() {
+  let p_host =  "smtp.gmail.com"; // smtp.elasticemail.com
+  let p_user =  "guisselle.hbo@gmail.com";
+  let p_password = "xxxxxxxxx"; //"DCD3ECC25EE4AE85C9EE4EFCA5C3FA949C46";
+  let p_from ="guisselle.hbo@gmail.com";
+  let p_to_email = document.getElementById('email').value;
+  let p_subject = "Tickets de RockMaster";
+  let p_body    = "Aquí recibirás tus boletos";
+
+  if (validarCampos() == 1) {
+    alert("[WARINING] Asegúrate que todos tus datos estén completos, de otra forma no se puede procesar la compra");
+    return;
+  };
+
+              Email.send({
+                Host: p_host,
+                Username: p_user,
+                Password: p_password ,
+                To: p_to_email,
+                From: p_from,
+                Subject: p_subject,
+                Body: p_body,
+              })
+                .then(function (message) {
+                  alert("Email Enviado a " + p_to_email);
+                });
+            };
